b602803f8ddb3bf912dbc9eeec244bf76437f778
Yuhao Ren <charlesren1996@gmail.com>
2020-05-02 09:17:16 +0800

    The auto-correction model.

---
+++ correction/data_process.py
@@ -0,0 +1,26 @@
+from torchtext.data import Field, BucketIterator
+import torch
+
+SRC = Field(tokenize = "spacy",
+            tokenizer_language="de",
+            init_token = '<sos>',
+            eos_token = '<eos>',
+            lower = True)
+
+TRG = Field(tokenize = "spacy",
+            tokenizer_language="en",
+            init_token = '<sos>',
+            eos_token = '<eos>',
+            lower = True)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+BATCH_SIZE = 128
+
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size = BATCH_SIZE,
+    device = device)
+
+
